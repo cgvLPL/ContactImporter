e7cbc8ed372f5d73b7417440c1814769e625a533
cgvLPL <redigo200107@gmail.com>
2026-08-26 19:23:40 +0700

    brand: use ContactImporter logotype in the app

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 (() => {
+  const applyBranding = () => {
+    const mobileBrand = document.querySelector('.mobile-brand');
+    if (mobileBrand) {
+      mobileBrand.innerHTML = '<img class="header-logotype" src="./assets/logo.svg" alt="ContactImporter">';
+    }
+
+    const brandHome = document.getElementById('brandHome');
+    if (brandHome) {
+      brandHome.innerHTML = '<img class="sidebar-logo-mark" src="./assets/logo-mark.svg" alt=""><span class="sidebar-brand-text">ContactImporter</span>';
+    }
+
+    if (!document.querySelector('link[rel="icon"]')) {
+      const favicon = document.createElement('link');
+      favicon.rel = 'icon';
+      favicon.type = 'image/svg+xml';
+      favicon.href = './assets/logo-mark.svg';
+      document.head.appendChild(favicon);
+    }
+
+    document.title = 'ContactImporter';
+  };
+
   const loadScript = (src) => new Promise((resolve, reject) => {
     const script = document.createElement('script');
     script.src = src;
@@ -6,6 +28,8 @@
     script.onerror = () => reject(new Error(`Failed to load ${src}`));
     document.body.appendChild(script);
   });
+
+  applyBranding();
 
   loadScript('./app-core.js')
     .then(() => loadScript('./app-export.js'))
