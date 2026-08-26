11263bccacdbb19ccacb607bfbd5c68700213aa9
cgvLPL <redigo200107@gmail.com>
2026-08-26 19:51:43 +0700

    ui: initialize tab workspace after app modules

--- app.js
+++ app.js
@@ -35,6 +35,7 @@
     .then(() => loadScript('./app-export.js'))
     .then(() => loadScript('./app-runtime-fix.js'))
     .then(() => loadScript('./column-mapping.js'))
+    .then(() => loadScript('./tab-ui.js'))
     .catch((error) => {
       console.error(error);
       const badge = document.getElementById('statusBadge');
